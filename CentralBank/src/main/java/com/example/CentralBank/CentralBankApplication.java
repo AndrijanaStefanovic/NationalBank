61724a06240078a26cd00d50f8ec164bb355045d
tamarakatic <tamarakatic@gmail.com>
2017-06-08 21:08:22 +0200

    Create new project

---
+++ CentralBank/src/main/java/com/example/CentralBank/CentralBankApplication.java
@@ -0,0 +1,12 @@
+package com.example.CentralBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralBankApplication.class, args);
+	}
+}
