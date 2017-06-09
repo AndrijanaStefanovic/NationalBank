0294c258882632613debaaaa3a1af396514658cf
tamarakatic <tamarakatic@gmail.com>
2017-06-09 20:38:30 +0200

    Create Company mvc

---
+++ Company/src/main/java/com/example/Company/controller/CompanyController.java
@@ -0,0 +1,33 @@
+package com.example.Company.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Company.model.BusinessPartner;
+import com.example.Company.model.Company;
+import com.example.Company.service.BusinessPartnerService;
+import com.example.Company.service.CompanyService;
+
+@RestController
+public class CompanyController {
+
+	@Autowired
+	private CompanyService companyService;
+	
+	@RequestMapping(
+			value = "/company/add",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.TEXT_PLAIN_VALUE
+			)
+	
+	public Company add(@RequestBody Company company) {
+		return companyService.add(company);
+	}
+	
+	
+}
